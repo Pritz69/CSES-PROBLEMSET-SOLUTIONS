91bf54bfda9fcf4c808e3314941cb3b9691e32ca
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-13 01:48:44 +0530

    Create 2 Apartments 2.py

---
+++ Sorting and Searching/2 Apartments 2.py
@@ -0,0 +1,18 @@
+n,m,k=map(int,input().split())
+l=list(map(int,input().split()))
+s=list(map(int,input().split()))
+l.sort()
+s.sort()
+i = 0
+j = 0
+c = 0
+while i < n and j < m:
+    if (l[i] - k) <= s[j] and s[j] <= (l[i] + k):
+        c += 1
+        i += 1
+        j += 1
+    elif l[i] > s[j]:
+        j += 1
+    else:
+        i += 1
+print(c)
